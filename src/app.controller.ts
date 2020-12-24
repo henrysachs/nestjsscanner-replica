f6f7f54d788dc146ce2736efc6a758fb8e64fd00
henrysachs <henrysachs@gmail.com>
2020-12-25 00:07:13 +0100

    another one

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const AWS_KEY = 'AKIAIOSFODNN7EXAMPLE';
     return this.appService.getHello();
   }
 }
--- src/scm-info.ts
+++ src/scm-info.ts
@@ -1,6 +1,8 @@
 import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
 import * as shell from 'shelljs';
+const fs = require('fs');
+
 @Processor('scan_scm')
 export class SCMConsumer {
   @Process()
@@ -9,13 +11,16 @@ export class SCMConsumer {
     console.log('test');
     console.log(job.data);
     // Run external tool synchronously
-    const result = await shell.exec(
-      'gitleaks --path=$(pwd) --config-path=/mnt/d/Projects/nestjsscanner-replica/.gitleaks/config.toml -v --report=my-report.json --unstaged',
-      { async: true },
+    const result = shell.exec(
+      'gitleaks --path=$(pwd) --config-path=/mnt/d/Projects/nestjsscanner-replica/.gitleaks/config.toml -v --report=my-report.json --redact',
     );
-    console.log(result.exitCode);
-    if (result.exitCode !== 0) {
+    if (result.code !== 0) {
+      console.log('leaks found');
+      let rawdata = fs.readFileSync('my-report.json');
+      let student = JSON.parse(rawdata);
+      console.log(student);
     }
+    console.log(result.code);
 
     return result;
   }
